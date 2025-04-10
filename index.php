be24d342b6fe1707a8ab161d5a0ec2bc04a37d16
Nos <zvezdazanatih@gmail.com>
2025-04-10 14:53:14 +0700

    w9

--- index.php
+++ index.php
@@ -1,26 +1,69 @@
-<?php 
+<?php
 echo "<center>PHP WORK!!!</center>";
 
-$servername = "localhost";
 
+$servername = "localhost";
 $username = "root";
 $password = "password";
-$connect = mysqli_connect($servername, $username, $password);
+$db_name = "expo";
+
+
+$connect = mysqli_connect($servername, $username, $password, $db_name);
 if (!$connect) {
     die("Ошибка подключения: " . mysqli_connect_error());
 }
-echo "Подключение к БД прошло успешно. Сервер: ", $servername, ", пользователь: ", $username;
+mysqli_set_charset($connect, "utf8");
+
+
+
+
+echo "Подключение к БД прошло успешно. Сервер: ", $servername, ", пользователь: ", $username."<br>";
+
+
+$sql = 'SELECT * FROM tab1';
+
+
+$result = mysqli_query($connect, $sql);
+
+
+#while ($row = mysqli_fetch_array($result)) {
+#    print("Имя: " . $row['nm'] . "; Возраст: . " . $row['age'] . "<br>");}
+
+
+print("<table border='1'>");
+
+
+print("<thead>
+    <tr>
+      <th>id</th>
+      <th>name</th>
+      <th>age</th>
+    </tr>
+  </thead>");
+
+
+
+
+print("<tbody>");
+
+
+while ($row = mysqli_fetch_array($result)) {
+    print("<tr>"."<td>".$row['id']."</td>"."<td>".$row['name']."</td>"."<td>".$row['age']."</td>"."</tr>");}
+
+
+
+
+print("</tbody>");
+print("</table>");
+
+
 mysqli_close($connect);
-
 ?>
+<h1> Добавь данных </h1>
+<form action="f.php" method="post" name="form1" style="background-color:gray;margin:10px;padding:20px;">
+    <p>Имя <input name="nam" type="text"> </p>
+    <p>Возраст <input name="ag" type="text"> </p>
+    <p><input name="submit" type="submit" value="push!"> </p>
 
 
-
-
-
-<?php
-echo "<br><br>Hello world!!!!!!!!!!!!!!<br>";
-echo "<p> hello from /trur/index.php </p><br>";
-
-phpinfo();
-?>
+</form>
