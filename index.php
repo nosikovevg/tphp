a8bcd1e4249fd21d37333611bf096c0e067e4617
Nos <zvezdazanatih@gmail.com>
2025-04-10 14:24:27 +0700

    w6

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
+<?php 
+echo "<center>PHP WORK!!!</center>";
+
+$servername = "localhost";
+
+$username = "root";
+$password = "password";
+$connect = mysqli_connect($servername, $username, $password);
+if (!$connect) {
+    die("Ошибка подключения: " . mysqli_connect_error());
+}
+echo "Подключение к БД прошло успешно. Сервер: ", $servername, ", пользователь: ", $username;
+mysqli_close($connect);
+
+?>
+
+
+
+
+
 <?php
-echo "Hello world!!!!!!!!!!!!!!<br>";
+echo "<br><br>Hello world!!!!!!!!!!!!!!<br>";
 echo "<p> hello from /trur/index.php </p><br>";
 
 phpinfo();
